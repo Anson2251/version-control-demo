6d3fb8e896e6390d00d8fe1293923a5501d2c3b8
Heyan Zhu <zhu_heyan@163.com>
2024-07-05 09:37:22 +0800

    initial commit

---
+++ plot-function.r
@@ -0,0 +1,6 @@
+plot_function <- function (points_to_plot) {
+    plot <- ggplot() +
+    geom_line(aes(x = 1:length(points_to_plot), y = points_to_plot, color = "blue"), linewidth = 0.7)
+
+    return(plot)
+}
